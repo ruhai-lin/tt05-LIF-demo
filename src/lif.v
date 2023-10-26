ea9b1110f2854f205ea097720147bf32737ef81c
rlin50 <rlin50@ucsc.edu>
2023-10-26 16:47:01 -0700

    test

---
+++ src/lif.v
@@ -0,0 +1,25 @@
+module lif (
+    input wire [7:0] current,
+    output wire [7:0] next_state,
+    output wire spike,
+    input wire clk,
+    input wire rst_n
+);
+    reg [7:0] state, threshold;
+
+    // resting potential and threshold
+    assign next_state = current + (state >> 1);
+    assign spike = (state >= threshold);
+
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            state <= 0;
+            threshold <= 32;
+        end else begin
+            state <= next_state;
+        end
+    end
+
+
+
+endmodule
