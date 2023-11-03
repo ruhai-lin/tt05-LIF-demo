e362572b6b6553366582c9b01b684111272b9fec
rlin50 <rlin50@ucsc.edu>
2023-11-03 15:05:24 -0700

    fixed more warnings

--- src/lif.v
+++ src/lif.v
@@ -9,11 +9,11 @@ module lif (
 );
 
     wire [7:0] next_state;
-    reg [7:0] threshold;  // 自适应阈值
-    reg [7:0] beta;
+    reg [15:0] threshold;
+    reg [15:0] beta;
 
-    parameter ADAPTIVE_INCREMENT = 295;  // 自适应增加因子
-    parameter ADAPTIVE_DECREMENT = 244;  // 自适应减小因子
+    parameter ADAPTIVE_INCREMENT = 295;  // adaptive increment factor
+    parameter ADAPTIVE_DECREMENT = 244;  // adaptive decrement factor
 
 
     always @(posedge clk) begin
@@ -25,15 +25,15 @@ module lif (
             if (spike) begin
                 state <= 0;
                 if (learnable_threshold && (threshold < 220))  // avoiding overflow 255
-                    threshold <= threshold * ADAPTIVE_INCREMENT >> 8;  // 增加自适应阈值
+                    threshold <= threshold * ADAPTIVE_INCREMENT >> 8;  // increase threshold
                 if (learnable_beta && (beta < 220))
-                    beta <= beta * ADAPTIVE_INCREMENT >> 8; // 增加自适应衰减率
+                    beta <= beta * ADAPTIVE_INCREMENT >> 8; // increse decay rate
             end else begin
                 state <= next_state;
                 if (learnable_threshold && (threshold > 8))  // avoiding being to low
-                    threshold <= threshold * ADAPTIVE_DECREMENT >> 8;  // 减小自适应阈值
+                    threshold <= threshold * ADAPTIVE_DECREMENT >> 8;  // decrease threshold
                 if (learnable_beta && (beta > 128))
-                    beta <= beta * ADAPTIVE_DECREMENT >> 8; // 增加自适应衰减率
+                    beta <= beta * ADAPTIVE_DECREMENT >> 8; // decrease decay rate
             end
         end
     end
--- src/tt_um_lif.v
+++ src/tt_um_lif.v
@@ -1,8 +1,8 @@
 `default_nettype none
 
 module tt_um_lif (
-    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
-    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input current
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 digit output
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
     output wire [7:0] uio_out,  // IOs: Bidirectional Output path
     output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
@@ -14,9 +14,10 @@ module tt_um_lif (
     // use bidirectionals as outputs
     assign uio_oe = 8'b11111111;
     assign uio_out [6:0] = 7'd0;
+    wire temp;
 
     // instantiate the lif neurons
-    lif lif1 (.current(ui_in), .state(uo_out), .spike(uio_out[7]), .learnable_threshold(1'b1), .learnable_beta(1'b1), .clk(clk), .rst_n(rst_n));
-    // lif lif2 (.current(ui_in), .state(uo_out), .spike(uio_out[7]), .clk(clk), .rst_n(rst_n));
+    lif lif1 (.current(ui_in), .state(uo_out), .spike(temp), .learnable_threshold(1'b1), .learnable_beta(1'b1), .clk(clk), .rst_n(rst_n));
+    // lif lif2 (.current(temp >> 6), .state(uo_out), .spike(uio_out[7]), .learnable_threshold(1'b1), .learnable_beta(1'b1), .clk(clk), .rst_n(rst_n));
 
 endmodule
