4de973f3df5e7b509d3cebcd88d8bcc0d466c67c
rlin50 <rlin50@ucsc.edu>
2023-11-02 20:48:25 -0700

    Add learnable threshold

--- src/lif.v
+++ src/lif.v
@@ -2,35 +2,40 @@ module lif (
     input wire [7:0] current,
     input wire clk,
     input wire rst_n,
+    input wire learnable_threshold,  // 控制阈值是否可以学习
+    // input wire learnable_beta,  // 控制decay rate是否可以学习
     output reg [7:0] state,
     output wire spike
 );
 
     wire [7:0] next_state;
     reg [7:0] threshold;  // 自适应阈值
+    // reg [7:0] beta;
 
     parameter ADAPTIVE_INCREMENT = 1.15;  // 自适应阈值增加因子
     parameter ADAPTIVE_DECREMENT = 0.95;  // 自适应阈值减小因子
 
+
     always @(posedge clk) begin
         if (!rst_n) begin
             state <= 8'b00000000;
-            threshold <= 8'b01100100;  // 初始自适应阈值100
+            threshold <= 8'b01100100;  // initial threshold 100
+            // beta <= 0.9;            // +
         end else begin
             if (spike) begin
                 state <= 8'b00000000;
-                if (threshold < 8'b11011100)  // 防止超过255
+                if (learnable_threshold && threshold < 8'b11011100)  // avoiding overflow 255 +
                     threshold <= threshold * ADAPTIVE_INCREMENT;  // 增加自适应阈值
             end else begin
                 state <= next_state;
-                if (threshold > 8'b00001000)  // 防止下降得过低
+                if (learnable_threshold && threshold > 8'b00001000)  // avoiding being to low +
                     threshold <= threshold * ADAPTIVE_DECREMENT;  // 减小自适应阈值
             end
         end
     end
 
     // resting potential and threshold
-    assign next_state = (spike ? 0 : current) + (spike > 0 ? 0 : (state * 7'b1110000 >> 7));
+    assign next_state = (spike ? 0 : (current)) + (spike > 0 ? 0 : (state * 7'b1110000 >> 7)); //+
     assign spike = (state >= threshold);
 
 endmodule
--- src/tt_um_lif.v
+++ src/tt_um_lif.v
@@ -16,6 +16,7 @@ module tt_um_lif (
     assign uio_out [6:0] = 7'd0;
 
     // instantiate the lif neurons
-    lif lif1 (.current(ui_in), .state(uo_out), .spike(uio_out[7]), .clk(clk), .rst_n(rst_n));
- 
+    lif lif1 (.current(ui_in), .state(uo_out), .spike(uio_out[7]), .learnable_threshold(1'b0), .clk(clk), .rst_n(rst_n));
+    // lif lif2 (.current(ui_in), .state(uo_out), .spike(uio_out[7]), .clk(clk), .rst_n(rst_n));
+
 endmodule
