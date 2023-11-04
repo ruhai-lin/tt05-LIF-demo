c93f0ff03a8355f527362a5a84b0f822a5e2db06
rlin50 <rlin50@ucsc.edu>
2023-11-03 22:36:11 -0700

    Docs updated.

--- src/tt_um_lif.v
+++ src/tt_um_lif.v
@@ -14,7 +14,6 @@ module tt_um_lif (
     // use bidirectionals as outputs
     assign uio_oe = 8'b11110000;
     assign uio_out [6:0] = 7'd0;
-    assign uio_in [7:2] = 6'd0;
 
     // instantiate the lif neurons
     lif lif1 (.current(ui_in), .state(uo_out), .spike(uio_out[7]), .adaptive_threshold(uio_in[0]), .adaptive_beta(uio_in[1]), .clk(clk), .rst_n(rst_n));
